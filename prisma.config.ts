bff48c9b5ed7ea5193fc87ecf88b14b0b5e18464
dafariski77 <rskdafa@gmail.com>
2026-06-16 19:45:16 +0700

    refactor: update prisma configuration to use env helper for database URL extraction

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'apps/user-wallet-service/prisma/schema.prisma',
   datasource: {
-    provider: 'postgresql',
-    url:
-      process.env.DATABASE_URL ||
-      'postgresql://postgres:postgres@localhost:5432/wallet_db?schema=public',
+    url: env('DATABASE_URL'),
   },
 });
